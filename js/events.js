3e317f00754f7d7016fb9e28be463ec6e125cc8d
Jason Hang <jasonxhang@gmail.com>
2018-06-20 21:00:38 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,19 +1,25 @@
 function getIt() {
   $("p").on("click", function() {
     alert("Hey!");
-  })
+  });
 }
 
 function frameIt() {
   $("img").on("load", function () {
     element.classList.add("tasty");
-  })
+  });
 }
 
 function pressIt() {
   $(input).on("keydown", function(key) {
-    if (key.which == )
-  })
+    if (key.which == 71) {
+      alert("g was pressed");
+    }
+  });
+}
+
+function submitIt(){
+  $
 }
 
 $(document).ready(function(){
